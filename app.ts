fee5ac34712926ec65cea4137fe24726f16ff766
Wade Nelson <nelson.wade.nelson@gmail.com>
2023-02-21 19:53:46 -0500

    cors fix

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import multer from 'multer';
+import cors from 'cors';
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/images', express.static('images'))
 const port = 3000;
 
